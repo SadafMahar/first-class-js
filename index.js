c12ec83363eac63a307805b0e841bc7ca5d9c616
Sadaf Mahar <169028965+SadafMahar@users.noreply.github.com>
2024-05-17 10:33:48 +0500

    first-commit

---
+++ index.js
@@ -0,0 +1,14 @@
+alert("hello world........");
+alert("hello sadaf........");
+alert("hello world");
+var name="sadaf";
+var Name="mahar";
+
+
+alert(Name + name)
+ first_Name= "s" ;
+ console.log(first_Name);
+ var first_Name = '1233' ;
+console.log(typeof(first_Name))
+var first_Name = 1233 ;
+console.log(typeof(first_Name))
